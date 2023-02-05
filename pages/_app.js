e18cdb54be828db9864ee3a1493f1da05d782f0d
手島香寿美 <info@cumak.net>
2023-02-06 00:14:38 +0900

    refactor: nprogress deletion

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,6 @@
-import '../assets/styles/style.scss'
-import React, { useEffect } from 'react'
-import nprogress from 'nprogress'
-
-// プログレスバーの設定(出ないけど)
-nprogress.configure({ showSpinner: false, speed: 400, minimum: 0.25 })
+import "../assets/styles/style.scss";
+import React from "react";
 
 export default function App({ Component, pageProps }) {
-  if (process.browser) {
-    // バーの表示開始
-    nprogress.start()
-  }
-  useEffect(() => {
-    // バーの表示終了
-    nprogress.done()
-  })
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
