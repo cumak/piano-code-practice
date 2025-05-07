4c11a9ae935b3fc5d4b114cfa44bfea30d98d59b
Kasumi Teshima <info@cumak.net>
2025-05-07 15:55:18 +0900

    fix: playwright.config

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,9 +4,9 @@ const baseURL =
   process.env.BASE_URL ||
   (process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : process.env.VERCEL_GIT_COMMIT_REF === "main"
+    : process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF === "main"
     ? process.env.BASE_DOMAIN
-    : process.env.VERCEL_GIT_COMMIT_REF === "develop"
+    : process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF === "develop"
     ? process.env.DEV_DOMAIN
     : "http://localhost:3000");
 
