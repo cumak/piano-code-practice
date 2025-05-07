eebff256c25ddc4cf47c57e7921133f6286f4670
Kasumi Teshima <info@cumak.net>
2025-05-07 17:47:51 +0900

    環境変数をNEXT_PUBLICに変更

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -4,13 +4,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const config = {
-  apiKey: process.env.FIREBASE_KEY,
-  authDomain: process.env.FIREBASE_DOMAIN,
-  databeseURL: process.env.FIREBASE_DATABASE,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_SENDER_ID,
-  appId: process.env.FIREBASE_APPID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_DOMAIN,
+  databeseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
 };
 
 const app = initializeApp(config);
